db36b0365493d2237465f9b6a9b45898073e2296
dsv <zxcv>
2024-10-11 19:23:01 +0500

    Initial Commit

---
+++ Mechanical Equipment/Mechanical_EquipmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mechanical_EquipmentApp.swift
+//  Mechanical Equipment
+//
+//  Created by Muhammad Rizwan on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mechanical_EquipmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
